3428ecd1f530d2df0102296df460d746feefb210
jkosha <nikita.koselev@gmail.com>
2020-07-02 18:17:00 +0100

    week 4 changes

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,4 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'pm-root',
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+  `
+})
+export class AppComponent {
+  pageTitle = 'Acme Product Management';
+}
+
+
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
@@ -8,3 +24,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular: Getting Started';
 }
+*/
