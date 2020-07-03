6ffeb136f312d064d7cfa132fdaa758a04bedd06
jkosha <nikita.koselev@gmail.com>
2020-07-03 17:45:06 +0100

    week 6 changes

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { ProductListComponent} from './products/product-list.component';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './products/product-list.component';
 
 @NgModule({
   declarations: [
@@ -9,8 +10,10 @@ import { ProductListComponent} from './products/product-list.component';
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
