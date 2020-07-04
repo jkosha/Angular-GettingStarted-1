ced40e1863749f70e27fbd11c31d20635edd3cec
jkosha <nikita.koselev@gmail.com>
2020-07-04 23:06:58 +0100

    chapter 7 completed

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -14,14 +14,3 @@ export class AppComponent {
 }
 
 
-/*import { Component } from '@angular/core';
-
-@Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Angular: Getting Started';
-}
-*/
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,11 +3,13 @@ import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {ProductListComponent} from './products/product-list.component';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
@@ -15,5 +17,4 @@ import {ProductListComponent} from './products/product-list.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
