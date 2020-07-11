13c74fdff380b91e1596cc5544ad23d0b0974985
jkosha <nikita.koselev@gmail.com>
2020-07-11 23:24:04 +0100

    week 13

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,21 +3,16 @@ import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {ProductListComponent} from './products/product-list.component';
-import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
-import {StarComponent} from './shared/star.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ProductDetailComponent} from './products/product-detail.component';
 import {RouterModule} from '@angular/router';
 import {WelcomeComponent} from './home/welcome.component';
 import {ProductDetailGuard} from './products/product-detail.guard';
+import {ProductModule} from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +28,8 @@ import {ProductDetailGuard} from './products/product-detail.guard';
       {path: 'welcome', component: WelcomeComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
